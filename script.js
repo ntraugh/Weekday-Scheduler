2cecb2dce6e2a036ca1ed5186313d4eed78daf37
Nate Traugh <n.traugh@yahoo.com>
2022-05-30 12:29:02 -0400

    storing all time slots correctly, saving on refresh but not redisplaying on refresh

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ var hour = parseInt(moment().format("H"))
 var tableRow = $(".row")
 var saveButton = $(".saveBtn")
 var inputText = $(".content")
+var area1 = $("#1")
+var area2 = $("#2")
+var area3 = $("#3")
+var area4 = $("#4")
+var area5 = $("#5")
+var area6 = $("#6")
+var area7 = $("#7")
+var area8 = $("#8")
+var area9 = $("#9")
 
 
 
@@ -38,8 +47,15 @@ Array.from(tableRow).forEach(row => {
 
 // function for storing our values into local storage
 function SaveInfo() {
-    localStorage.setItem("text info", inputText.val())
-
+    localStorage.setItem("9AM", area1.val()) // gives the 9AM slot a key and value pair, but nothing else
+    localStorage.setItem("10AM", area2.val())
+    localStorage.setItem("11AM", area3.val())
+    localStorage.setItem("12PM", area4.val())
+    localStorage.setItem("1PM", area5.val())
+    localStorage.setItem("2PM", area6.val())
+    localStorage.setItem("3PM", area7.val())
+    localStorage.setItem("4PM", area8.val())
+    localStorage.setItem("5PM", area9.val())
 };
 
 // creating a for loop to add an event listener to every save button
